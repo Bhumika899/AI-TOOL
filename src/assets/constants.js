8e68996dffa2db582748758d98f0da2289961303
bhumika_03 <bhumikasabharwal0@gmail.com>
2026-06-07 17:33:28 +0530

    AI-TOOL

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,2 +1,2 @@
 export const URL =
-    "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=AQ.Ab8RN6I-VWaImfMDQIYK4qr4qE2Qhi5BUFcz7CeNBivrFXjBNg";
+    "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=";
